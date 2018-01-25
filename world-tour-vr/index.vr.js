45216388b316509d280ea000c803c6d8ccc56530
Farid Ansari <farid12ansari7@gmail.com>
2018-01-25 18:52:08 +0530

    added menu items

--- world-tour-vr/index.vr.js
+++ world-tour-vr/index.vr.js
@@ -3,7 +3,8 @@ import { AppRegistry, asset, Pano, Text, View, StyleSheet } from 'react-vr';
 
 export default class world_tour_vr extends Component {
   state = {
-    showMenu: false
+    showMenu: false,
+    places: ['A', 'B', 'C', 'D']
   };
 
   toggleMenuHandler = () => {
@@ -15,24 +16,60 @@ export default class world_tour_vr extends Component {
   };
 
   render() {
+    let menuItems = this.state.showMenu
+      ? this.state.places.map((place, index) => (
+          <View
+            key={index}
+            style={[
+              styles.menuItem,
+              {
+                transform: [
+                  {
+                    translate: [
+                      0,
+                      index % ((this.state.places.length) / 2),
+                      0
+                    ]
+                  }
+                ]
+              }
+            ]}
+          >
+            <Text style={styles.menuItemText}>{place}</Text>
+          </View>
+        ))
+      : null;
     return (
       <View>
         <Pano source={asset('bac.jpg')} />
         <View style={styles.menuButton}>
           <Text
             onEnter={this.toggleMenuHandler}
-            onExit={this.toggleMenuHandler}
+            
             style={[styles.buttonText, { fontSize: 0.1 }]}
           >
             {this.state.showMenu ? 'Close Menu' : 'Open Menu'}
           </Text>
         </View>
+        <View style={styles.menu}>{menuItems}</View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  menu: {
+    width: 5,
+    height: 1.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    transform: [
+      {
+        translate: [-3, 0, -7.5]
+      }
+    ]
+  },
   menuButton: {
     width: 0.5,
     height: 0.5,
@@ -47,6 +84,20 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'red',
     textAlign: 'center'
+  },
+  menuItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 1.2,
+    width: 1.2,
+    borderRadius: 0.5,
+    borderWidth: 0.02,
+    backgroundColor: '#fff'
+  },
+  menuItemText: {
+    fontSize: 0.2,
+    textAlign: 'center',
+    color: 'blue'
   }
 });
 
